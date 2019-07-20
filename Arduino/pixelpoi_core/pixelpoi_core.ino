5648f6e8518724be26733fdff3c1e58765c5697e
Zetacat <k94155@gmail.com>
2019-07-21 00:37:04 +0800

    add image mode and fixed mode changing bug

--- Arduino/pixelpoi_core/pixelpoi_core.ino
+++ Arduino/pixelpoi_core/pixelpoi_core.ino
@@ -1,6 +1,7 @@
 #include <FastLED.h>
 #include <SoftwareSerial.h>
 #include "characters.h"
+#include "images.h"
 
 #define LED_PIN 7
 #define BUTTON_PIN 5
@@ -8,6 +9,8 @@
 #define DEFAULT_COLOR CRGB(50, 50, 50)
 #define CHAR_WIDTH 5
 #define CHAR_HEIGHT 8
+#define IMG_WIDTH 16
+#define IMG_HEIGHT 16
 
 /* for BT */
 SoftwareSerial BT(8, 9); // for bluetooth serial
@@ -15,7 +18,8 @@ String strReceive = ""; // record received string
 int terminated = 0; // indicate whether the string is finished
 
 /* for leds */
-int blink_interval = 32;
+int blink_interval = 8;
+int printOffset = 0; // currently only for printing letters
 CRGB leds[NUM_LEDS];
 
 /* for button and mode switching */
@@ -32,79 +36,10 @@ int indexDisplay = 1; // 0: custom  1~4: demo1~4 (helloworld, pangram, clock, im
 
 
 
-int printable(char c){
-  return (int)c>=32 && (int)c<=126;
-}
-
-
-void printLetter(char letter, CRGB color){
-  if (!printable(letter)){
-      return;
-    }
-  letter -= 32;
-  
-  // print the letter
-  for (int indexCol=0; indexCol<CHAR_WIDTH; indexCol++){
-    for (int indexRow=0; indexRow<CHAR_HEIGHT; indexRow++){
-          bool current_bit = bitRead(font[letter][indexCol], 7-indexRow);
-          if (current_bit)
-            leds[indexRow] = color;
-          else
-            leds[indexRow] = CRGB(0, 0, 0);
-          Serial.print(current_bit);
-    }
-    Serial.println();
-    FastLED.show();
-    delay(blink_interval);
-  }
-
-  // print the gap between letters
-  for (int i=0; i<NUM_LEDS; i++){
-    leds[i] = CRGB(0, 0, 0);
-    Serial.print(0);
-  }
-  Serial.println();
-  FastLED.show();
-  delay(blink_interval);
-}
-
-
-void printString(String &str){
-  for (int index=0; index<(int)(str.length()); index++){
-    printLetter(str[index], DEFAULT_COLOR);
-  }
-}
-
-
-
-class Demo{
-  public:
-  Demo();
-  static void Helloworld();
-  static void Pangram();
-};
-void Demo::Helloworld(){
-  static String strHelloworld = "Hello World! ";
-  printString(strHelloworld);
-}
-void Demo::Pangram(){
-  static String strPangram = "The quick onyx goblin jumps over the lazy dwarf. ";
-  printString(strPangram);
-}
-
-
-
-void setup() {
-  Serial.begin(9600);
-  BT.begin(9600);
-  Serial.println("BT is ready!");
-  FastLED.addLeds<WS2812B, LED_PIN, RGB>(leds, NUM_LEDS);
-  pinMode(BUTTON_PIN, INPUT); // pin 5 is for mode switching button
-}
-
-void loop() {
+int btnHandler(){
   /* button handling */
   int btnRead = digitalRead(BUTTON_PIN); // read the state of the pushbutton value:
+  int valReturn = 0; // if indexDisplay or mode is changed return 1, otherwise 0
 
   if (btnRead != btnState && ((millis()-lastValidChangeTimeStamp)>bounceDelay)){
     
@@ -122,21 +57,162 @@ void loop() {
       }else{
         mode = 0;
       }
+      valReturn = 1;
     }
     else{
       // long press
       if (mode == 0){
         mode = 1;
+        valReturn = 1;
       }
     }
     
     btnState = btnRead;
     lastValidChangeTimeStamp = millis();
   }
+  return valReturn;
+}
+
+
+
+int printImage(byte img[][16]){
+  int terminate = 0;
+  
+  // print the image
+  for (int indexCol=0; indexCol<IMG_WIDTH; indexCol++){
+    for (int indexRow=0; indexRow<IMG_HEIGHT; indexRow++){
+      leds[indexRow] = colorTable[img[indexCol][indexRow]];
+    }
+    FastLED.show();
+    delay(blink_interval);
+
+    // button handling
+    terminate = btnHandler();
+    if (terminate){
+      return terminate;
+    }
+  }
+
+  // print the gap between columns
+  for (int i=0; i<NUM_LEDS; i++){
+    leds[i] = CRGB(0, 0, 0);
+  }
+  FastLED.show();
+  delay(blink_interval);
+
+  // button handling
+  terminate = btnHandler();
+  if (terminate){
+    return terminate;
+  }
+
+  return 0;
+}
+
+
+
+int printable(char c){
+  return (int)c>=32 && (int)c<=126;
+}
+
+
+int printLetter(char letter, CRGB color){
+  int terminate = 0;
+  if (!printable(letter)){
+      return;
+    }
+  letter -= 32;
+  
+  // print the letter
+  for (int indexCol=0; indexCol<CHAR_WIDTH; indexCol++){
+    for (int indexRow=0; indexRow<CHAR_HEIGHT; indexRow++){
+          bool current_bit = bitRead(font[letter][indexCol], 7-indexRow);
+          if (current_bit)
+            leds[indexRow + printOffset] = color;
+          else
+            leds[indexRow + printOffset] = CRGB(0, 0, 0);
+          //Serial.print(current_bit);
+    }
+    //Serial.println();
+    FastLED.show();
+    delay(blink_interval);
+
+    // button handling
+    terminate = btnHandler();
+    if (terminate){
+      return terminate;
+    }
+  }
+
+  // print the gap between letters
+  for (int i=0; i<NUM_LEDS; i++){
+    leds[i] = CRGB(0, 0, 0);
+    //Serial.print(0);
+  }
+  //Serial.println();
+  FastLED.show();
+  delay(blink_interval);
+
+  // button handling
+  terminate = btnHandler();
+  if (terminate){
+    return terminate;
+  }
+
+  return 0;
+}
+
+
+void printString(String &str){
+  for (int index=0; index<(int)(str.length()); index++){
+    int terminate = printLetter(str[index], DEFAULT_COLOR);
+    // quit if mode is changed before string finish
+    if (terminate){
+      delay(1000);
+      return;
+    }
+  }
+}
+
+
+
+class Demo{
+  public:
+  Demo();
+  static void Helloworld();
+  static void Pangram();
+  static void MySuperG();
+};
+void Demo::Helloworld(){
+  static String strHelloworld = "Hello World! ";
+  printString(strHelloworld);
+}
+void Demo::Pangram(){
+  static String strPangram = "The quick onyx goblin jumps over the lazy dwarf. ";
+  printString(strPangram);
+}
+void Demo::MySuperG(){
+  printImage(img);
+}
+
+
+
+void setup() {
+  Serial.begin(9600);
+  BT.begin(9600);
+  Serial.println("BT is ready!");
+  FastLED.addLeds<WS2812B, LED_PIN, RGB>(leds, NUM_LEDS);
+  pinMode(BUTTON_PIN, INPUT); // pin 5 is for mode switching button
+}
+
+void loop() {
+  /* button handling */
+  btnHandler();
   
   
   /* bluetooth data-transfer mode */
   if (mode == 1){
+    Serial.println("BT mode");
     char buf = 0;
     // data transfer from serial port to BT
     if (Serial.available()){
@@ -170,16 +246,21 @@ void loop() {
 
   /* Display mode */
   if (mode == 0){
+    Serial.print("Current mode: 0  Page: ");
+    Serial.println(indexDisplay);
     switch(indexDisplay){
       case 0:
         break;
       case 1:
         Demo::Helloworld();
         break;
-      case 3:
+      case 2:
         Demo::Pangram();
         break;
+      case 3:
+        break;
       case 4:
+        Demo::MySuperG();
         break;
     }
   }
